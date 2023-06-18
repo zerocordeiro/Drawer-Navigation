3731edc02232259073de4f22bab8f41112e7e922
Nishant Desai <you@example.com>
2023-06-18 15:48:35 +0530

    first commit

--- AppStack.js
+++ AppStack.js
@@ -1,8 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./Screens/HomeScreen";
-import Login from "./Screens/Login";
 import CustomDrawer from "./Components/CustomDrawer";
 const Drawer = createDrawerNavigator();
 const AppStack = () => {
@@ -13,11 +11,8 @@ const AppStack = () => {
         component={HomeScreen}
         options={{ headerShown: false }}
       />
-      <Drawer.Screen name="Login" component={Login} />
     </Drawer.Navigator>
   );
 };
 
 export default AppStack;
-
-const styles = StyleSheet.create({});
